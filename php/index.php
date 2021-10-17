f49074710cc59920210a38576a8fdc09331193bd
CG-TI192FR <ceaglei.grigorii@isa.utm.md>
2021-10-17 10:35:12 +0300

    added file

---
+++ php/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Game-news - компьютерные игры, новости, мнения</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="header">
+			<a class="logo" href="index.php">
+				<img class="main_logo" src="img/logo.png", width="90", height="90">
+			</a>
+			<div>
+				<ul id="menu">
+					<li>
+						<a href="news.php">Новости</a>
+					</li>
+					<li>
+						<a href="articles.php">Статьи</a>
+					</li>
+					<li>
+						<a href="games.php">Игры</a>
+					</li>
+					<li>
+						<a href="about.php">О нас</a>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<ul id="auth">
+					<li>
+						Войти
+					</li>
+				</ul>
+			</div>
+		</div>
+	</header>
+
+	<footer>
+
+	</footer>
+</body>
+</html>
